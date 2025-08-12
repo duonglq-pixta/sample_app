e64c2d67e6b49b811ec35d445a4ffaacc1861807
Le Quy Duong <lequyduong@Pixta-admins-MacBook-Pro.local>
2025-08-12 17:13:58 +0700

    finish: Chapter 5 - Filling in the layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/help"
-  get "static_pages/about"
+  root "static_pages#home"
+  get "help" => "static_pages#help"
+  get "about" => "static_pages#about"
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -11,8 +12,4 @@ Rails.application.routes.draw do
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-
-  # Defines the root path route ("/")
-  # root "posts#index"
-  root "static_pages#home"
 end
