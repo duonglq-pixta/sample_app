63453c2008de9045df78449bd55581239c6c7838
Le Quy Duong <lequyduong@Pixta-admins-MacBook-Pro.local>
2025-08-13 17:32:10 +0700

    finish: Chapter 8 - Log in

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   get "help" => "static_pages#help"
   get "about" => "static_pages#about"
   get "signup" => "users#new"
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+  get "/logout", to: "sessions#destroy"
 
   get "up" => "rails/health#show", as: :rails_health_check
   resources :users
