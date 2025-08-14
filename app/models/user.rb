3ffddffd54128e1b9b55228cfec3ba62e109a9a0
Le Quy Duong <lequyduong@Pixta-admins-MacBook-Pro.local>
2025-08-14 14:26:06 +0700

    finish: Chapter 9 - Advanced login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+    attr_accessor :raw_remember_token
+    
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :name, presence: true, length: { maximum: 50 }
     validates :email, presence: true, length: { maximum: 255 },
@@ -7,4 +9,32 @@ class User < ApplicationRecord
     before_save { self.email = email.downcase }
     has_secure_password
     validates :password, length: { minimum: 6 }
+
+    def User.new_token
+        SecureRandom.urlsafe_base64
+    end
+
+    def User.digest(string)
+        cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+        BCrypt::Password.create(string, cost: cost)
+    end
+
+    def remember
+        raw_token = User.new_token
+        self.raw_remember_token = raw_token
+        update_attribute(:remember_token, User.digest(raw_token))
+    end
+    
+    def authenticated?(remember_token)
+        return false if remember_token.nil?
+        return false if self.remember_token.nil?
+        BCrypt::Password.new(self.remember_token).is_password?(remember_token)
+    rescue BCrypt::Errors::InvalidHash
+        false
+    end
+
+    def forget
+        update_attribute(:remember_token, nil)
+        self.raw_remember_token = nil
+    end
 end
