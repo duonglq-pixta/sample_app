6e9c0aac9d8cf2ce1233045add0bffca25568ec8
Le Quy Duong <lequyduong@Pixta-admins-MacBook-Pro.local>
2025-08-14 14:26:06 +0700

    finish: Chapter 9 - Advanced login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,14 +6,24 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    if user&.authenticate(params[:session][:password])
+    if user && user.authenticate(params[:session][:password])
+      reset_session
+      
+      if params[:session][:remember_me] == '1'
+        remember user
+      else
+        forget user
+      end
+
       log_in user
       redirect_to user
     else
-      flash.now[:danger] = 'Invalid email/password combination'
+      flash.now[:danger] = t("flash.sessions.create.error")
       render 'new', status: :unprocessable_entity
     end
   end
+
+
 
   def destroy
     log_out if logged_in?
