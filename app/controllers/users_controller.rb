ea73500be032c27e6f559f9022afb3d71033ce5e
Le Quy Duong <duong.lequy@pixta.co.jp>
2025-08-15 16:19:24 +0700

    finish: Chapter 11 - Account Activation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,26 +2,29 @@ class UsersController < ApplicationController
   before_action :logged_in_user, only: [:edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update]
   before_action :admin_user, only: :destroy
+  before_action :activated_user, only: [:index, :show]
   def new
     @user = User.new
   end
   
   def show
     @user = User.find(params[:id])
+    redirect_to root_url and return unless @user.activated?
   rescue ActiveRecord::RecordNotFound
     flash[:error] = t("flash.users.show.error")
     redirect_to users_path
   end
   
   def index
-    @users = User.paginate(page: params[:page], per_page: 2)
+    @users = User.where(activated: true).paginate(page: params[:page], per_page: 2)
   end
   
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = t("flash.users.create.success")
-      redirect_to @user
+      @user.send_activation_email
+      flash[:info] = "Please check your email to activate your account."
+      redirect_to root_url
     else
       render :new, status: :unprocessable_entity
     end
@@ -73,5 +76,12 @@ class UsersController < ApplicationController
 
   def admin_user
     redirect_to(root_url) unless current_user.admin?
+  end
+
+  def activated_user
+    unless current_user&.activated?
+      flash[:warning] = t("flash.users.not_activated")
+      redirect_to root_url
+    end
   end
 end
